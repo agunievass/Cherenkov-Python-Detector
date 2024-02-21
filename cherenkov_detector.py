851f3431062592881131b44e1e0296895930d293
AgusN <agunievas12@gmail.com>
2024-02-21 16:53:46 -0300

    Added some image operations to extend exposure

--- cherenkov_detector.py
+++ cherenkov_detector.py
@@ -7,6 +7,8 @@ cap.set(cv.CAP_PROP_AUTO_WB, 0.00)
 cap.set(cv.CAP_PROP_SATURATION, 3.00)
 cap.set(cv.CAP_PROP_BRIGHTNESS, -1000.00)
 
+ret, last_frame = cap.read()
+
 if not cap.isOpened():
     print("Cannot open camera") #prints an error if couldnt open the camera
     exit()
@@ -15,14 +17,19 @@ while True:
     e1 = cv.getTickCount() #used for tracking performance
     ret, frame = cap.read() #Capture one frame of the webcam, its a jpg image
     
-    cv.imshow('Camera View', frame) #display the image onto a window
     
+    exposed_frame = cv.add(frame, last_frame)
+    
+    cv.imshow('Camera View', frame) #display the image onto a window
+    cv.imshow('Exposed Frames', exposed_frame) 
 
+    last_frame = frame
+    
     if cv.waitKey(1) == ord('q'): #press "q" to quit the program
         break
     e2 = cv.getTickCount() #used for tracking performance
     time_running = (e2 - e1) / cv.getTickFrequency()
     print(time_running)
-
+    
 cap.release()
 cv.destroyAllWindows() #stops recording and deletes all windows
